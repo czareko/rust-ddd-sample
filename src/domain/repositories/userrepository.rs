aee56f9c0768f6cdd04e510a9b72329950fd4726
Cezary Olborski <cezary.olborski@gmail.com>
2022-09-10 08:44:29 +0800

    feat: domain - repository trait

---
+++ src/domain/repositories/userrepository.rs
@@ -0,0 +1,11 @@
+use crate::domain::entities::user::User;
+use mockall::predicate::*;
+use mockall::*;
+
+#[automock]
+pub trait UserRepository {
+    fn by_id(&self, id: &str) -> Result<User, String>;
+    fn save(&self, user: User);
+    fn next_identity(&self) -> String;
+    fn all(&self) -> Vec<User>;
+}
